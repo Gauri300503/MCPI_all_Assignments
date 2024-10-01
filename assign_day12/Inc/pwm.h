2d1f18ec2952afab38fb79e4f439069e373afebd
Gauri shete <gaurishete220@gmail.com>
2024-10-02 00:25:48 +0530

    Assignment day12 done

---
+++ assign_day12/Inc/pwm.h
@@ -0,0 +1,19 @@
+/*
+ * pwm.h
+ *
+ *  Created on: Oct 2, 2024
+ *      Author: gauri
+ */
+
+#ifndef PWM_H_
+#define PWM_H_
+
+
+#include"stm32f4xx.h"
+
+#define PS 16;
+#define ARR_VAL 100
+void PwmInit();
+
+
+#endif /* PWM_H_ */
---
+++ assign_day12/Inc/switch.h
@@ -0,0 +1,27 @@
+/*
+ * switch.h
+ *
+ *  Created on: Oct 2, 2024
+ *      Author: gauri
+ */
+
+#ifndef SWITCH_H_
+#define SWITCH_H_
+
+
+
+#include"stm32f4xx.h"
+
+#define SWITCH_PORT GPIOA
+#define SWITCH_PIN 0
+#define SWITCH_CLK 0
+
+#define SWITCH_EXTI 0
+
+void SwitchInit();
+void EXTI0_IRQHandler();
+
+extern volatile uint32_t flag;
+
+
+#endif /* SWITCH_H_ */
